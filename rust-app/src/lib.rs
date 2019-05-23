c79012fa2ca30234469e1233bac2b33f848a7873
Chris Redmon <credmonster@gmail.com>
2019-05-23 14:47:58 -0400

    rust-app: Add sample code that requires std::iter::{IntoIterator, Iterator} and std::option::Option

--- rust-app/src/lib.rs
+++ rust-app/src/lib.rs
@@ -11,6 +11,18 @@ pub extern "C" fn hello_rust() {
         writeln!(&mut std::io::Stdout, "Boxed value {}", boxed).unwrap();
     }
 
+    {
+        let io_results: [std::io::Result<()>; 2] = [Ok(()), Err(std::io::Error)];
+
+        for io in &io_results {
+            if io.is_ok() {
+                writeln!(&mut std::io::Stdout, "io::Result is Ok").unwrap();
+            } else {
+                writeln!(&mut std::io::Stdout, "io::Result is Error").unwrap();
+            }
+        }
+    }
+
     zephyr::kernel::k_thread_user_mode_enter(|| {
         zephyr::user::k_str_out("Hello from Rust userspace with forced user-mode syscall\n");
         zephyr::any::k_str_out("Hello from Rust userspace with runtime-detect syscall\nNext call will crash if userspace is working.\n");
