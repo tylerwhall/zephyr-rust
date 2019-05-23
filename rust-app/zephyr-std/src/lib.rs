8282a34e61a90dc7f7729563a437944b4b372486
Chris Redmon <credmonster@gmail.com>
2019-05-23 14:45:29 -0400

    zephyr-std: Add std::option::Option

--- rust-app/zephyr-std/src/lib.rs
+++ rust-app/zephyr-std/src/lib.rs
@@ -57,6 +57,13 @@ pub mod io {
     pub struct Error;
 }
 
+pub mod option {
+    #![stable(feature = "rust1", since = "1.0.0")]
+
+    #[stable(feature = "rust1", since = "1.0.0")]
+    pub use core::option::Option;
+}
+
 pub mod prelude;
 
 mod panicking;
