b0ff3d58b87cba2c483243093d3497744a2e12f2
Chris Redmon <credmonster@gmail.com>
2019-05-23 15:22:27 -0400

    rust-app: Add test for std::ops::{Range, RangeFrom, RangeFull, RangeInclusive, RangeTo, RangeToInclusive}

--- rust-app/src/lib.rs
+++ rust-app/src/lib.rs
@@ -23,6 +23,18 @@ pub extern "C" fn hello_rust() {
         }
     }
 
+    // test std::ops::{Range, RangeFrom, RangeFull, RangeInclusive, RangeTo, RangeToInclusive}
+    {
+        let a: [u8; 4] = [1, 2, 3, 4];
+        let len = a.iter().len();
+        for _ in &a[0..len] {}
+        for _ in &a[0..=(len - 1)] {}
+        for _ in &a[..] {}
+        for _ in &a[0..] {}
+        for _ in &a[..len] {}
+        for _ in &a[..=(len - 1)] {}
+    }
+
     zephyr::kernel::k_thread_user_mode_enter(|| {
         zephyr::user::k_str_out("Hello from Rust userspace with forced user-mode syscall\n");
         zephyr::any::k_str_out("Hello from Rust userspace with runtime-detect syscall\nNext call will crash if userspace is working.\n");
