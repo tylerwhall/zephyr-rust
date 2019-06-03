a1a863837b1cd69fe5bf6fdd9c535fb343fea8c6
Tyler Hall <tylerwhall@gmail.com>
2019-06-03 16:41:31 -0400

    zephyr: checked time conversions from duration

--- rust-app/zephyr/src/time.rs
+++ rust-app/zephyr/src/time.rs
@@ -1,3 +1,6 @@
+use core::convert::{TryFrom, TryInto};
+use core::fmt;
+use core::num::TryFromIntError;
 use core::time::Duration;
 
 #[derive(Copy, Clone, PartialEq, Eq, PartialOrd, Ord, Debug, Hash, From)]
@@ -17,17 +20,26 @@ impl InstantMs {
     }
 
     pub fn checked_add_duration(self, other: &Duration) -> Option<Self> {
-        self.checked_add(other.into())
+        let other = other.try_into().ok()?;
+        self.checked_add(other)
     }
 
     pub fn checked_sub_duration(self, other: &Duration) -> Option<Self> {
-        self.checked_sub(other.into())
+        let other = other.try_into().ok()?;
+        self.checked_sub(other)
     }
 }
 
-impl From<&Duration> for InstantMs {
-    fn from(dur: &Duration) -> Self {
-        InstantMs(dur.as_secs() as i64 * 1000 + dur.subsec_millis() as i64)
+impl TryFrom<&Duration> for InstantMs {
+    type Error = TryFromDurationError;
+
+    fn try_from(dur: &Duration) -> Result<Self, Self::Error> {
+        let secs = i64::try_from(dur.as_secs())?;
+        let ms = i64::from(dur.subsec_millis());
+        secs.checked_mul(1000)
+            .and_then(|secs| secs.checked_add(ms))
+            .ok_or(TryFromDurationError(()))
+            .map(InstantMs)
     }
 }
 
@@ -48,5 +60,41 @@ impl From<DurationMs> for Duration {
         let secs = dur.0 / 1000;
         let ms = (dur.0 % 1000) as u32;
         Duration::new(secs as u64, ms * 1000 * 1000)
+    }
+}
+
+impl TryFrom<&Duration> for DurationMs {
+    type Error = TryFromDurationError;
+
+    fn try_from(dur: &Duration) -> Result<Self, Self::Error> {
+        let secs = i32::try_from(dur.as_secs())?;
+        let ms = i32::try_from(dur.subsec_millis())?;
+        secs.checked_mul(1000)
+            .and_then(|secs| secs.checked_add(ms))
+            .ok_or(TryFromDurationError(()))
+            .map(DurationMs)
+    }
+}
+
+/*
+#[test]
+fn try_from_duration() {
+    let ms = DurationMs::try_from(Duration::new(50, 6 * 1000 * 1000)).unwrap();
+    assert_eq!(ms, DurationMs(50 * 1000 + 6));
+}
+*/
+
+#[derive(Clone, Copy, Debug)]
+pub struct TryFromDurationError(());
+
+impl fmt::Display for TryFromDurationError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        "Duration to ms conversion out of range".fmt(f)
+    }
+}
+
+impl From<TryFromIntError> for TryFromDurationError {
+    fn from(_e: TryFromIntError) -> Self {
+        TryFromDurationError(())
     }
 }
