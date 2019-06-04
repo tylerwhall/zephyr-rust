f340b2da94f3aceb06ef4b814e64e8abe03c3c13
Tyler Hall <tylerwhall@gmail.com>
2019-06-04 18:12:15 -0400

    build.sh: use a different cargo target dir for sysroot
    Sometimes rust finds duplicate libraries between the target dir and
    those copied to the sysroot.

--- rust-app/build.sh
+++ rust-app/build.sh
@@ -1,11 +1,14 @@
 #!/bin/sh -ex
 
+HOST=$(rustc -vV | grep host: | cut -d ' ' -f 2)
 VERSION="+nightly-2019-05-22"
-cargo ${VERSION} -v build --target=${RUST_TARGET} --target-dir=${CARGO_TARGET_DIR} --release --manifest-path=./sysroot/Cargo.toml -p std
+cargo ${VERSION} -v build --target=${RUST_TARGET} --target-dir=${CARGO_TARGET_DIR}/sysroot-build --release --manifest-path=./sysroot/Cargo.toml -p std
 
 SYSROOT_LIB="${SYSROOT}/lib/rustlib/${RUST_TARGET}/lib"
-mkdir -p ${SYSROOT_LIB}
-cp -a ${CARGO_TARGET_DIR}/${RUST_TARGET}/release/deps/* ${SYSROOT_LIB}
+SYSROOT_LIB_HOST="${SYSROOT}/lib/rustlib/${HOST}/lib"
+mkdir -p ${SYSROOT_LIB} ${SYSROOT_LIB_HOST}
+cp -a ${CARGO_TARGET_DIR}/sysroot-build/${RUST_TARGET}/release/deps/* ${SYSROOT_LIB}
+cp -a ${CARGO_TARGET_DIR}/sysroot-build/release/deps/* ${SYSROOT_LIB_HOST}
 
 export RUSTFLAGS="${RUSTFLAGS} --sysroot ${SYSROOT}"
 cargo ${VERSION} -v build --target=${RUST_TARGET} --target-dir=${CARGO_TARGET_DIR} --release
