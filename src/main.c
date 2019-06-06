45d5e34bd92c424a2c8d4a26afae4f4a451f8ee3
Nathan Jones <nathan.jones@lexmark.com>
2019-06-06 13:14:38 -0400

    zephyr: implement thread local storage

--- src/main.c
+++ src/main.c
@@ -10,6 +10,15 @@
 extern uint8_t hello_rust(void);
 extern void hello_rust_user(void);
 
+extern uint8_t hello_rust_second_thread(void *, void *, void *);
+
+#define MY_STACK_SIZE 500
+#define MY_PRIORITY 5
+
+K_THREAD_DEFINE(my_tid, MY_STACK_SIZE,
+		hello_rust_second_thread, NULL, NULL, NULL,
+		MY_PRIORITY, 0, K_NO_WAIT);
+
 void main(void)
 {
         hello_rust();
