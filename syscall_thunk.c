71136afa8fed4b7b3cd07ec41fbf0f7ee5faf60e
Chris Redmon <credmonster@gmail.com>
2019-06-07 13:57:42 -0400

    syscall_thunk: Define RUST_CONFIG_USERSPACE variable
    Addresses undefined symbol for zephyr_sys::raw::RUST_CONFIG_USERSPACE.

--- syscall_thunk.c
+++ syscall_thunk.c
@@ -3,3 +3,10 @@
 #include <kernel.h>
 #include <device.h>
 #include <uart.h>
+
+// Create a constant we can use from Rust in all cases
+#ifdef CONFIG_USERSPACE
+const bool RUST_CONFIG_USERSPACE = true;
+#else
+const bool RUST_CONFIG_USERSPACE = false;
+#endif
