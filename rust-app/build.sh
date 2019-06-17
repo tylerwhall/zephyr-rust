5840c717b89e10bab94745d9aee10ad4c41ef29c
Tyler Hall <tylerwhall@gmail.com>
2019-06-17 17:55:00 -0400

    Convert to module. Allows external Rust apps

--- rust-app/build.sh
+++ rust-app/build.sh
@@ -2,13 +2,22 @@
 
 HOST=$(rustc -vV | grep host: | cut -d ' ' -f 2)
 VERSION="+nightly-2019-05-22"
-cargo ${VERSION} -v build --target=${RUST_TARGET} --target-dir=${CARGO_TARGET_DIR}/sysroot-build --release --manifest-path=./sysroot/Cargo.toml -p std
+CARGO_ARGS="${VERSION} -v build --target=${RUST_TARGET} --release"
+cargo ${CARGO_ARGS} --target-dir=${SYSROOT_BUILD} --manifest-path=./sysroot/Cargo.toml -p std
 
 SYSROOT_LIB="${SYSROOT}/lib/rustlib/${RUST_TARGET}/lib"
 SYSROOT_LIB_HOST="${SYSROOT}/lib/rustlib/${HOST}/lib"
-mkdir -p ${SYSROOT_LIB} ${SYSROOT_LIB_HOST}
-cp -a ${CARGO_TARGET_DIR}/sysroot-build/${RUST_TARGET}/release/deps/* ${SYSROOT_LIB}
-cp -a ${CARGO_TARGET_DIR}/sysroot-build/release/deps/* ${SYSROOT_LIB_HOST}
+
+copy_dir_if_changed() {
+    mkdir -p $2
+    if ! diff --brief --recursive $1 $2; then
+        rm -r $2
+        cp -a $1 $2
+    fi
+}
+
+copy_dir_if_changed ${SYSROOT_BUILD}/${RUST_TARGET}/release/deps ${SYSROOT_LIB}
+copy_dir_if_changed ${SYSROOT_BUILD}/release/deps ${SYSROOT_LIB_HOST}
 
 export RUSTFLAGS="${RUSTFLAGS} --sysroot ${SYSROOT}"
-cargo ${VERSION} -v build --target=${RUST_TARGET} --target-dir=${CARGO_TARGET_DIR} --release
+cargo ${CARGO_ARGS} --target-dir=${APP_BUILD} --manifest-path=${CARGO_MANIFEST}
