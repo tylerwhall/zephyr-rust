21364d64300e627b040f461a97842d48fe55ea5a
Tyler Hall <tylerwhall@gmail.com>
2019-06-18 23:27:51 -0400

    Kconfig: select whether to use a mem pool or kmalloc
    Not automatically tied into the default global allocator yet.

--- rust-smem.c
+++ rust-smem.c
@@ -1,9 +1,3 @@
-/*
- * Copyright (c) 2012-2014 Wind River Systems, Inc.
- *
- * SPDX-License-Identifier: Apache-2.0
- */
-
 #include <zephyr.h>
 #include <init.h>
 #include <misc/mempool.h>
@@ -18,10 +12,14 @@ K_APPMEM_PARTITION_DEFINE(rust_std_partition);
 #define RUST_STD_SECTION .data
 #endif
 
-#define RUST_STD_MEM_POOL_SIZE 1024
-SYS_MEM_POOL_DEFINE(rust_std_mem_pool, NULL, 16,
-                    RUST_STD_MEM_POOL_SIZE, 1, 4, RUST_STD_SECTION);
+#if defined(CONFIG_RUST_ALLOC_POOL)
+SYS_MEM_POOL_DEFINE(rust_std_mem_pool, NULL, CONFIG_RUST_HEAP_MEM_POOL_MIN_SIZE,
+                    CONFIG_RUST_HEAP_MEM_POOL_SIZE, 1, 4, RUST_STD_SECTION);
+#elif CONFIG_HEAP_MEM_POOL_SIZE == 0
+#error CONFIG_HEAP_MEM_POOL_SIZE (k_malloc) must be non-zero if not using a Rust sys mem pool.
+#endif
 
+#if defined(CONFIG_USERSPACE) || defined(CONFIG_RUST_ALLOC_POOL)
 static int rust_std_init(struct device *arg)
 {
     ARG_UNUSED(arg);
@@ -30,9 +28,12 @@ static int rust_std_init(struct device *arg)
     k_mem_domain_init(&rust_std_domain, 0, NULL);
     k_mem_domain_add_partition(&rust_std_domain, &rust_std_partition);
 #endif
+#ifdef CONFIG_RUST_ALLOC_POOL
     sys_mem_pool_init(&rust_std_mem_pool);
+#endif
 
     return 0;
 }
 
 SYS_INIT(rust_std_init, PRE_KERNEL_2, CONFIG_APPLICATION_INIT_PRIORITY);
+#endif
