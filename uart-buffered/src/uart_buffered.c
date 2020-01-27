12cefcfb33124bd9547aa998c4f64edaecf93ba8
Tyler Hall <tylerwhall@gmail.com>
2020-01-27 17:38:54 -0500

    uart_buffered: log errors and add traces

--- uart-buffered/src/uart_buffered.c
+++ uart-buffered/src/uart_buffered.c
@@ -2,8 +2,11 @@
 
 #include <kernel.h>
 #include <uart.h>
+#include <logging/log.h>
 
 #include "uart_buffered.h"
+
+LOG_MODULE_REGISTER(uart_buffered);
 
 /* TX interrupt handler */
 static void uart_buffered_tx(struct uart_buffered_tx *uart)
@@ -36,6 +39,7 @@ static void uart_buffered_tx(struct uart_buffered_tx *uart)
 void uart_buffered_rx_timeout(struct k_timer *timer)
 {
 	struct k_poll_signal *signal = k_timer_user_data_get(timer);
+	LOG_DBG("rx wake on timer");
 	k_poll_signal_raise(signal, 0);
 }
 
@@ -49,6 +53,10 @@ static void uart_buffered_rx(struct uart_buffered_rx *uart)
 		u8_t c;
 		if (uart_fifo_read(fifo->device, &c, 1) == 1) {
 			fifo_push(fifo, c);
+			LOG_DBG("uart byte 0x%x write = %d read = %d used = %d\n",
+				c, fifo->fifo->write,
+				fifo->fifo->read,
+				fifo->fifo->write - fifo->fifo->read);
 		} else {
 			/* Still want to receive more (fifo not full) */
 			disable_irq = false;
@@ -57,15 +65,18 @@ static void uart_buffered_rx(struct uart_buffered_rx *uart)
 	}
 
 	if (disable_irq) {
+		LOG_DBG("disable rx irq");
 		uart_irq_rx_disable(fifo->device);
 	}
 
 	if (fifo_used(fifo) >= fifo_capacity(fifo) / 2) {
 		/* Wake reader now if more than half full */
 		k_timer_stop(uart->timer);
+		LOG_DBG("rx wake");
 		k_poll_signal_raise(fifo->signal, 0);
 	} else if (!fifo_empty(fifo)) {
 		/* Make sure reader is woken eventually if any data is available */
+		LOG_DBG("rx timer start");
 		k_timer_start(uart->timer, K_MSEC(1), 0);
 	}
 }
@@ -74,8 +85,14 @@ static void uart_buffered_rx(struct uart_buffered_rx *uart)
 void uart_buffered_irq(struct device *uart, struct uart_buffered_rx *rx_fifo,
 		       struct uart_buffered_tx *tx_fifo)
 {
-	uart_irq_update(uart);
-	if (uart_irq_is_pending(uart)) {
+	while (uart_irq_update(uart) && uart_irq_is_pending(uart)) {
+		int err = uart_err_check(uart);
+		if (err & UART_ERROR_OVERRUN) {
+			LOG_ERR("overrun");
+		}
+		if (err & UART_ERROR_FRAMING) {
+			LOG_ERR("framing");
+		}
 		if (uart_irq_rx_ready(uart)) {
 			__ASSERT(uart == rx_fifo->fifo.device,
 				 "mismatched uart and fifo");
@@ -143,5 +160,6 @@ void uart_buffered_init(struct uart_buffered *buffered, struct device *uart,
 	uart_buffered_tx_init(&buffered->tx, uart);
 
 	uart_irq_callback_set(uart, irq_handler);
+	uart_irq_err_enable(uart);
 	uart_irq_rx_enable(uart);
 }
