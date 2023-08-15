9f01c8afab480bc63d1fb7ce7693d1127a186d1f
Tyler Hall <tylerwhall@gmail.com>
2023-08-15 15:16:12 -0400

    ci: increase parallelism

--- ci/build-all.sh
+++ ci/build-all.sh
@@ -5,7 +5,7 @@
 ZEPHYR_VERSIONS="2.7.3 2.6.0 2.5.0 2.4.0 2.3.0"
 
 parallel \
-    -j1 \
+    -j8 \
     --results log/container \
     --resume \
     --halt now,fail=1 \
@@ -14,7 +14,7 @@ parallel \
 
 # First build the main sample for zephyr versions
 parallel \
-    -j4 \
+    -j8 \
     --results log/build \
     --resume \
     --halt now,fail=1 \
@@ -25,7 +25,7 @@ parallel \
 
 # Full set. --resume removes duplicates from above
 parallel \
-    -j4 \
+    -j8 \
     --results log/build \
     --resume \
     ZEPHYR_VERSION={1} ./build-cmd.sh west build -d /tmp/build -p auto -b {2} {3} \
