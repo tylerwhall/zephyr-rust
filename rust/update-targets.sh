054fc6407b43a1c6e31f83ef68b42b787dddce15
Tyler Hall <tylerwhall@gmail.com>
2024-03-13 12:43:43 -0400

    Create a script for generating target json
    The json is not stable across rust versions, and all of these are out of
    date. It's also very time consuming to handle these manually, given the
    number of targets.
    
    This could certainly be generated during the build, but riscv needs to
    be handled generically and it would require `jq` in the build
    environment.

---
+++ rust/update-targets.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+targets=(
+armv7r-zephyr-eabihf
+armv7r-zephyr-eabi
+i686-unknown-zephyr
+riscv32imac-unknown-zephyr-elf
+# riscv32ima-unknown-zephyr-elf # need to update script to generically handle RISCV extensions. Manually update from above for now.
+riscv32imc-unknown-zephyr-elf
+riscv64imac-unknown-zephyr-elf
+thumbv7em-zephyr-eabihf
+thumbv7em-zephyr-eabi
+thumbv7m-zephyr-eabi
+)
+
+for target in "${targets[@]}"; do
+    # Replace "zephyr" with "none"
+    rust_target="${target/zephyr/none}"
+
+    # Special case mapping for i686-unknown-none
+    case $rust_target in
+    i686-unknown-none)
+        rust_target=i686-unknown-linux-gnu
+        extra_filter='| .["features"] = "-mmx,-sse,+soft-float"'
+        ;;
+    armv7r-*)
+        extra_filter='| .["features"] += ",+v7,+thumb-mode,+thumb2,+rclass"'
+        ;;
+    *)
+        extra_filter=""
+        ;;
+    esac
+    echo "Updating $target from $rust_target"
+    # Set target-family and os to zephyr
+    # Remove is-builtin, linker, linker-flavor
+    filter='.["target-family"] = "zephyr" |
+        .["os"] = "zephyr" |
+        .["position-independent-executables"] = "false" |
+        .["relocation-model"] = "static" |
+        .["dynamic-linking"] = "false" |
+        .["has-thread-local"] = "false" |
+        del(.["is-builtin"])'
+    filter="$filter $extra_filter"
+    RUSTC_BOOTSTRAP=1 rustc --print target-spec-json -Z unstable-options --target $rust_target |
+        jq "$filter" > $target.json
+done
